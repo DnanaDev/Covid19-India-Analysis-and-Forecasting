30dbb67253cac337031dc37467f4c95824162d91
Anand <anand.2700@yahoo.in>
2020-04-16 13:46:00 +0530

    Created API Data Module.

---
+++ india_API_data/Covid19_india_org_api.py
@@ -0,0 +1,85 @@
+"""
+Querying a public API for JSON data
+Using Covid19india.org api to get daily cases, states, etc.
+
+Data Format: JSON
+data in the format of ['cases_time_series'] with objects for each day nested in it. ['key_values']
+another key/object in it of the form ['statewise']
+
+Data starting from 30th January to the current day.
+
+"""
+
+import json
+from urllib.request import urlopen
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+# got the json data but it is not very readable
+# print(source)
+
+# print(json.dumps(data, indent=2))
+
+# data in the format of ['cases_time_series'] with objects for each day nested in it. ['key_values']
+# another key/object in it of the form ['statewise']
+# Lets read cases time-series
+# "dailydeceased": "0",
+# "dailyrecovered": "0",
+# "date": "30 January ",
+# "totalconfirmed": "1",
+# "totaldeceased": "0",
+# "totalrecovered": "0"
+
+
+# print(json.dumps(data['cases_time_series'], indent = 2))
+
+
+def list_cases_stat(json_obj, column):
+    """
+    This function takes in the JSON retrieved from the API and returns a list of integers for the specified
+    column, can be used to then plot the time-series.
+    :param json_obj: The JSON object returned by the API.
+    :param column: The metric that is to be returned, dailyconfirmed etc.
+    :return: Returns a list of integers of the time series for the specified stat.
+    """
+    stat_list = []
+    for daily_num in json_obj['cases_time_series']:
+        stat_list.append(daily_num[column])
+    try:
+        stat_list_int = [int(x) for x in stat_list]
+    except ValueError:
+        stat_list_int = stat_list
+
+    return stat_list_int
+
+
+def make_dataframe():
+    """"Makes Dataframe with parsed data and saves it as CSV and returns it.
+    Args:
+    Returns:Dataframe. With Columns DailyConfirmed, DailyDeceased, DailyRecovered."""
+
+    # Fetching The JSON
+    with urlopen("https://api.covid19india.org/data.json") as response:
+        source = response.read()
+        data = json.loads(source)
+
+    # Getting Data From Json Object using list_cases_stat function
+    daily_conf = list_cases_stat(data, 'dailyconfirmed')
+    daily_dec = list_cases_stat(data, 'dailydeceased')
+    daily_rec = list_cases_stat(data, 'dailyrecovered')
+    total_conf = list_cases_stat(data, 'totalconfirmed')
+    total_dec = list_cases_stat(data, 'totaldeceased')
+    total_rec = list_cases_stat(data, 'totalrecovered')
+
+    list_dates = list_cases_stat(data, 'date')
+
+
+    dataframe = pd.DataFrame(
+        {'Date': list_dates, 'DailyConfirmed': daily_conf, 'DailyDeceased': daily_dec, 'DailyRecovered': daily_rec,
+         'TotalConfirmed': total_conf, 'TotalDeceased': total_dec, 'TotalRecovered': total_rec})
+
+    dataframe.to_csv('COVID_India_Updated_from_API.csv', index=False)
+
+    return dataframe
